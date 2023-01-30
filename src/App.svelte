d377c83331f3eaa1b1a9e2512b4a4ae0127403a2
nisten <nisten@give2science.com>
2023-01-30 13:41:41 -0500

    restyling navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import {
 		Container,
+		Collapse,
 		Navbar,
 		NavbarToggler,
 		NavbarBrand,
 		Nav,
 		NavItem,
 		NavLink,
+		Dropdown,
+		DropdownToggle,
+		DropdownMenu,
+		DropdownItem,
 		Row,
 		Col,
 	} from "sveltestrap";
@@ -16,9 +21,14 @@
 	import { Chart, LineSeries } from "svelte-lightweight-charts";
 
 	let isOpen = false;
+
+	function handleUpdate(event){
+		isOpen = event.detail.isOpen;
+	}
+
 	const toggle = () => (isOpen = !isOpen);
 	const data = [
-        { time: '2019-04-11', value: 80.01 },
+        { time: '2019-04-11', value: 180.01 },
         { time: '2019-04-12', value: 96.63 },
         { time: '2019-04-13', value: 76.64 },
         { time: '2019-04-14', value: 81.89 },
@@ -27,8 +37,39 @@
         { time: '2019-04-17', value: 96.63 },
         { time: '2019-04-18', value: 76.64 },
         { time: '2019-04-19', value: 81.89 },
-        { time: '2019-04-20', value: 74.43 },
+        { time: '2019-04-20', value: 94.43 },
+		{ time: '2019-05-11', value: 30.01 },
+        { time: '2019-05-12', value: 96.63 },
+        { time: '2019-05-13', value: 26.64 },
+        { time: '2019-05-14', value: 91.89 },
+        { time: '2019-05-15', value: 74.43 },
+        { time: '2019-05-16', value: 30.01 },
+        { time: '2019-05-17', value: 16.63 },
+        { time: '2019-05-18', value: 96.64 },
+        { time: '2019-05-19', value: 81.89 },
+        { time: '2019-05-20', value: 274.43 },
+		{ time: '2019-06-11', value: 80.01 },
+        { time: '2019-06-12', value: 96.63 },
+        { time: '2019-06-13', value: 76.64 },
+        { time: '2019-06-14', value: 111.89 },
+        { time: '2019-06-15', value: 84.43 },
+        { time: '2019-06-16', value: 40.01 },
+        { time: '2019-06-17', value: 96.63 },
+        { time: '2019-06-18', value: 76.64 },
+        { time: '2019-06-19', value: 11.89 },
+        { time: '2019-06-20', value: 84.43 },
+		{ time: '2019-07-11', value: 120.01 },
+        { time: '2019-07-12', value: 86.63 },
+        { time: '2019-07-13', value: 76.64 },
+        { time: '2019-07-14', value: 11.89 },
+        { time: '2019-07-15', value: 84.43 },
+        { time: '2019-07-16', value: 70.01 },
+        { time: '2019-07-17', value: 76.63 },
+        { time: '2019-07-18', value: 46.64 },
+        { time: '2019-07-19', value: 21.89 },
+        { time: '2019-07-20', value: 314.43 },
     ]
+
 	//IndexedDB storage wrapper
 	//Typescript interface
 	interface Docs {
@@ -59,19 +100,34 @@
 </script>
 
 <main>
-	<Container fluid>
-		<Row class="mb-1">
-			<Navbar color="dark" dark fixed="top">
-				<NavbarBrand href="/" class="me-auto">🦝rakun.ai</NavbarBrand>
-				<NavbarToggler on:click={toggle} class="me-2" id="options" aria-label="options click" />
-			</Navbar>
-		</Row>
-	<Row>1</Row>
-	<Row>2</Row>
+	<Navbar color="dark" dark fixed="top" expand="md" class="navbary" >
+		<NavbarBrand href="/">🦝rakun.ai</NavbarBrand>
+		<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+		<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		  <Nav class="ms-auto" navbar>
+			<NavItem>
+			  <NavLink href="#about/">About us</NavLink>
+			</NavItem>
+			<NavItem>
+			  <NavLink href="#solutions/">Solutions</NavLink>
+			</NavItem>
+			<Dropdown nav inNavbar>
+			  <DropdownToggle nav caret>Technology</DropdownToggle>
+			  <DropdownMenu end>
+				<DropdownItem href="https://github.com/openai/whisper">Whisper</DropdownItem>
+				<DropdownItem href="https://github.com/dexie/Dexie.js">Dexie.js</DropdownItem>
+				<DropdownItem divider />
+				<DropdownItem href="https://github.com/pyscript/pyscript">Pyscript</DropdownItem>
+			  </DropdownMenu>
+			</Dropdown>
+		  </Nav>
+		</Collapse>
+	</Navbar>
 	<Row class='row-fluid'>
 		<Col>meow</Col>
-    	<Col>.col-sm-auto .offset-sm-1</Col>
+    	
 	</Row>
+
 	<!-- <py-script>
 		from datetime import datetime
 		now = datetime.now()
@@ -82,13 +138,18 @@
 		<LineSeries data={data}/>
 	</Chart>
 	</Row>
-	</Container>
 </main>
 <style>
 	:global(body) {
+		padding-top: 60px;
 		background: white;
 		display: flex;
 		align-items: center;
 		justify-content: center;
 	}
+	:navbary {
+		padding-inline: 20px;
+
+	}
+
 </style>
